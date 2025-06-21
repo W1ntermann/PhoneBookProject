11b5c485c7d8722a8f5aa63285299f9571f63ead
Bohdan Hembatiuk <bogdangembatyuk@gmail.com>
2025-06-21 13:29:40 +0300

    Initial commit

---
+++ PhoneBookGeneric/Program.cs
@@ -0,0 +1,19 @@
+﻿using PhoneBookGeneric.BaseServices;
+using PhoneBookGeneric.Models;
+using PhoneBookGeneric.Repositories;
+
+IRepository<Subscriber> repository = new InMemoryRepository<Subscriber>();
+ILogger logger = new ConsoleLogger();
+PhoneBook<Subscriber> book = new PhoneBook<Subscriber>(repository, logger);
+
+book.AddSubscriber(new Subscriber
+{
+    Id = 1,
+    Name = "Badan",
+    LastName = "Badanowycz",
+    Country = "Ukraine",
+    City = "Odesa",
+    PhoneNumber = "380-XXX-XX-XX"
+});
+
+book.Get();
